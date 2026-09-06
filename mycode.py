0a15960178f55b1c668f072dc6e6dbae9acb45dd
keerthi-180205 <keerthin180205@gmail.com>
2026-09-06 14:19:42 +0530

    1-commit before dvc

---
+++ mycode.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+
+df = {
+    "Name" : ["Keerthi", "Keerthi N", "Keerthi N Kumar"],
+    "Age" : [21, 22, 18],
+    "City" : ["Bangalore", "Mysore", "Tumkur"]
+}
+
+df = pd.DataFrame(df)
+
+data_dir = 'data'
+
+os.makedirs(data_dir, exist_ok=True)
+
+# define path
+
+file_path = os.path.join(data_dir, 'sample_data.csv')
+
+df.to_csv(file_path, index=False)
+
+print(f"csv file saved to {file_path}")
